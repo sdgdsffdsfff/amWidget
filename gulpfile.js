8d970afcd7d93f6bda16a949525b8cfe62c69947
轩与 <wangwei.wang@alipay.com>
2014-08-19 13:01:29 +0800

    增加对spm的支持

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+	rename = require('gulp-rename'),
 	amTransportGulp = require('gulp-am-transport'),
 	uglify = require('gulp-uglify'),
 	amwDest = require('gulp-amw-dest');
@@ -14,6 +15,7 @@ gulp.task('build', function () {
 gulp.task('buildCMD', function () {
 	gulp.src('./src/gallery/**/*.js')
 		.pipe(amTransportGulp({family: "AW", loader: "cmd", uglify: true}))
+		.pipe(rename({extname: ".cmd.js"}))
 		.pipe(amwDest('./dist/', "cmd"));
 	gulp.src('./src/gallery/**/README.md')
 		.pipe(amwDest('./dist/', "cmd"));
